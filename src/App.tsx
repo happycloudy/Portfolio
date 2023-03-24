0734aa36b768ea7226d7c8b5fd74694f969542b0
happycloudy <arion.mikola@gmail.com>
2023-03-25 01:34:03 +0300

    folder moved and added home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Header} from "./modules/Header";
+import HomeSection from "./sections/HomeSection/HomeSection";
 
 function App() {
   return (
-    <>
+    <main>
         <Header/>
-    </>
+        <HomeSection/>
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {GlobalStyles} from "./modules/Global/styles/GlobalStyles";
+import {GlobalStyles} from "./styles/GlobalStyles";
 import {ThemeProvider} from "styled-components";
-import {theme} from "./modules/Global/styles/theme";
+import {theme} from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
