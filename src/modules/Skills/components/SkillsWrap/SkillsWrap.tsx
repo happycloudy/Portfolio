a5a939b097738f5b68c425ffbdeb6d65c2751c9b
happycloudy <arion.mikola@gmail.com>
2023-03-30 00:39:28 +0300

    fixed skills animations

--- src/modules/Skills/components/SkillsWrap/SkillsWrap.tsx
+++ src/modules/Skills/components/SkillsWrap/SkillsWrap.tsx
@@ -1,19 +1,44 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+
+const animatedLeftReveal = keyframes`
+  0% {
+    transform: translate(-100px);
+    opacity: 1;
+  }
+  
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`
+
+const animatedRightReveal = keyframes`
+  from{
+    transform: translate(100px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`
+
 
 export const SkillsWrap = styled.div`
   height: 100%;
   width: 80vw;
   padding-top: 7vh;
   transition-delay: 0.5s;
-  opacity: 0;
   
   &.right {
     margin-left: 20vw;
-    opacity: 1;
+    opacity: 0;
+    animation: ${animatedRightReveal} ${({theme}) => theme.timings.normal} forwards 0.5s;
   }
   
   &.left {
     margin-left: 0;
-    opacity: 1;
+    opacity: 0;
+    animation: ${animatedLeftReveal} ${({theme}) => theme.timings.normal} forwards 0.5s;
   }
 `
